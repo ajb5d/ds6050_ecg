5845579471bd3b57ea3683cc7d000a03ca80b221
Andrew Barros <ajb5d@virginia.edu>
2023-10-03 10:14:05 -0400

    fix formatting of output file name

--- preprocess_data.py
+++ preprocess_data.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 
 BASE_ECG_PATH = Path("/scratch/ajb5d/ecg/mimic-iv-ecg-diagnostic-electrocardiogram-matched-subset-1.0/")
 BASE_DATA_PATH = Path("./data/")
+OUTPUT_DATA_PATH = Path('/scratch/ajb5d/ecg/tfrecords')
 
 def wfdb_to_example(rec):
     r = wfdb.rdrecord(BASE_ECG_PATH / rec.path)
@@ -32,10 +33,25 @@ def wfdb_to_example(rec):
     example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString() 
 
+def output_file(target, file_count):
+    retrun str(OUTPUT_DATA_PATH / f"{target}-{file_count:04}.tfrecords")
+
 for target in ['test', 'train', 'val']:
     ecg_data = pd.read_csv(BASE_DATA_PATH / f"{target}_ecgs.csv")
-    with tf.io.TFRecordWriter(str(BASE_DATA_PATH / f"{target}.tfrecords")) as writer:
-        for r in tqdm(ecg_data.itertuples(), total = len(ecg_data)):
-            ex = wfdb_to_example(r)
-            if ex is not None:
-                writer.write(ex)
+    record_count = 0
+    file_count = 0
+
+    writer = tf.io.TFRecordWriter(output_file(target, file_count))
+    for r in tqdm(ecg_data.itertuples(), total = len(ecg_data)):
+        ex = wfdb_to_example(r)
+        if ex is not None:
+            writer.write(ex)
+            record_count += 1
+
+        if record_count > 512:
+            record_count = 0
+            file_count += 1
+            writer.close()
+            writer = tf.io.TFRecordWriter(output_file(target, file_count))
+
+    writer.close()
